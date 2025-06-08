dc1cbdc934ecf8fa7746cc578dd8a8917ebe503a
Gyusung Lee <ko76562385@gmail.com>
2025-06-08 10:08:14 +0900

    메세지 주고받기

--- chatapp-back/utils/io.js
+++ chatapp-back/utils/io.js
@@ -1,3 +1,4 @@
+const chatController = require("../Controllers/chat.controller");
 const userController = require("../Controllers/user.controller");
 
 module.exports = function (io) {
@@ -7,7 +8,23 @@ module.exports = function (io) {
     socket.on("login", async (userName, cb) => {
       try {
         const user = await userController.saveUser(userName, socket.id);
+        const welcomeMessage = {
+          chat: `${user.name} 님이 방에 입장했습니다.`,
+          user: {id: null, name: "system"},
+        };
+        io.emit("message", welcomeMessage);
         cb({ok: true, data: user});
+      } catch (error) {
+        cb({ok: false, error: error.message});
+      }
+    });
+
+    socket.on("sendMessage", async (message, cb) => {
+      try {
+        const user = await userController.checkUser(socket.id);
+        const newMessage = await chatController.saveChat(message, user);
+        io.emit("message", newMessage);
+        cb({ok: true});
       } catch (error) {
         cb({ok: false, error: error.message});
       }
--- chatapp-client/src/App.js
+++ chatapp-client/src/App.js
@@ -1,10 +1,17 @@
 import {useEffect, useState} from "react";
 import "./App.css";
 import socket from "./server";
-
+import InputFieId from "./components/InputField/InputField";
+import MessageContainer from "./components/MessageContainer/MessageContainer";
 function App() {
   const [user, setUser] = useState(null);
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
   useEffect(() => {
+    socket.on("message", (message) => {
+      setMessageList((prevState) => prevState.concat(message));
+    });
     askUserName();
   }, []);
   const askUserName = () => {
@@ -17,9 +24,22 @@ function App() {
       }
     });
   };
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (!user) {
+      alert("로그인 후 메시지를 보낼 수 있습니다.");
+      return;
+    }
+    socket.emit("sendMessage", message, (res) => {
+      console.log("sendMessage res", res);
+    });
+  };
   return (
     <div>
-      <div className="App"></div>
+      <div className="App">
+        <MessageContainer messageList={messageList} user={user} />
+        <InputFieId message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
     </div>
   );
 }
